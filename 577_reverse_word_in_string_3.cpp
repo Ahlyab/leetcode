f73984396a77e4f5d9616ab8cede7ae1c5719d34
unknown <ahlyabasad@gmail.com>
2023-10-01 12:34:26 +0500

    Problem 577 added

---
+++ 577_reverse_word_in_string_3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res = "";
+        int curr = 0;
+        for(int i=0; i<s.size(); ++i) {
+            if(s[i] == ' ') {                
+                for(int j=i-1; j >=curr ; --j) {
+                    res += s[j];
+                }
+                res += s[i];
+                curr  = i+1;
+
+            }
+        }
+
+        for(int i = s.size()-1; i >= curr; --i){
+            res += s[i];
+        }
+
+        return res;
+    }
+};
