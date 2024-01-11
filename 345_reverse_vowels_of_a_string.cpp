ce08a49329a63c5b422df1cc067768c55f068b41
Ahlyab <ahlyabasad@gmail.com>
2024-01-11 12:00:40 +0500

    problem 345 added

---
+++ 345_reverse_vowels_of_a_string.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    string reverseVowels(string s)
+    {
+        int i = 0, j = s.size();
+        while (i < j)
+        {
+            if (isVowel(s[i]) && isVowel(s[j]))
+            {
+                swap(s[i], s[j]);
+                ++i;
+                --j;
+            }
+            else if (!isVowel(s[i]))
+            {
+                ++i;
+            }
+            else if (!isVowel(s[j]))
+            {
+                --j;
+            }
+        }
+        return s;
+    }
+
+    bool isVowel(char c)
+    {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+};
