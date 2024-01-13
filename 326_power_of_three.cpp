6f6f94e1526f00f09a4772b655363f77c7388450
Ahlyab <ahlyabasad@gmail.com>
2024-01-13 11:57:46 +0500

    problem 326 added

---
+++ 326_power_of_three.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        unsigned int i = 1;
+        if (n < 0)
+        {
+            return false;
+        }
+        while (i <= n)
+        {
+            if (i == n)
+            {
+                return true;
+            }
+
+            i *= 3;
+        }
+
+        return false;
+    }
+};
