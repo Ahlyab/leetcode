93cf4e2071e3f2cd7636978f85d95a201074a106
Ahlyab <ahlyabasad@gmail.com>
2024-01-17 11:54:34 +0500

    problem 27 added

---
+++ 27_remove_element.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+public:
+    int removeElement(vector<int> &vec, int val)
+    {
+        vec.erase(std::remove(vec.begin(), vec.end(), val), vec.end());
+        return vec.size();
+    }
+};
