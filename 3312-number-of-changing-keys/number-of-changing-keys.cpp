2fd597406a003b520c4f916800e653de721e012e
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-03-18 10:00:23 +0500

    Time: 0 ms (100.00%) | Memory: 8 MB (24.72%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.cpp
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int countKeyChanges(string s) {
         transform(s.begin(), s.end(), s.begin(), ::tolower);
-        int count   = 0;
+        int count = 0;
         char LU = s[0];
-        for(char c : s) {
-            if(c != LU) {
+        for (char &c : s) {
+            if (c != LU) {
                 ++count;
                 LU = c;
-            } 
+            }
         }
 
         return count;
-
     }
 };
