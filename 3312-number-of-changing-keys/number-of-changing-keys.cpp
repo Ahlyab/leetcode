49f2949dc66a0a481b37c3b99dade0ed5663f7f9
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-03-18 09:57:10 +0500

    Time: 6 ms (11.66%) | Memory: 8 MB (62.79%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        int count   = 0;
+        char LU = s[0];
+        for(char c : s) {
+            if(c != LU) {
+                ++count;
+                LU = c;
+            } 
+        }
+
+        return count;
+
+    }
+};
