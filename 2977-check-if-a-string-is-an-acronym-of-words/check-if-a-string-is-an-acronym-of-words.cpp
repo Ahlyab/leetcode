88e287d8bd5c6ed7bc010aaafbf6eddd56c8f8a5
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-03-30 12:26:41 +0500

    Time: 15 ms (39.53%) | Memory: 43 MB (90.84%) - LeetSync

--- 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
+++ 2977-check-if-a-string-is-an-acronym-of-words/check-if-a-string-is-an-acronym-of-words.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     bool isAcronym(vector<string>& words, string s) {
-        string res = "";
-        for(int i=0; i<words.size(); ++i) {
-            res += words[i][0];
+        // string res = "";
+        // for(int i=0; i<words.size(); ++i) {
+        //     res += words[i][0];
+        // }
+        if(s.size() != words.size()) {
+            return false;
+        }
+        int i=0;
+        for(; i<s.size(); ++i) {
+            if(s[i] != words[i][0]) {
+                return false;
+            }
         }
 
-        return res == s;
+        return i == words.size();
     }
 };
