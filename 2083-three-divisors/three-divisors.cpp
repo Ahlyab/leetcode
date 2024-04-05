726ed9eb2c696d28e39e618c26f72e061c74894f
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-05 12:49:31 +0500

    Time: 0 ms (100.00%) | Memory: 7.1 MB (63.88%) - LeetSync

--- 2083-three-divisors/three-divisors.cpp
+++ 2083-three-divisors/three-divisors.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isThree(int n) {
         int c = 2;
-        for(int i=2; i<=n/2; ++i) {
+        int s = n/2;
+        for(int i=2; i<=s; ++i) {
             if(n%i==0) {
                 ++c;
             }
