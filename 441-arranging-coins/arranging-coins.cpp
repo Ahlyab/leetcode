951110dab4fbbf4bbd0eab57defe53368d3508a1
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-05 13:52:31 +0500

    Time: 12 ms (9.20%) | Memory: 7.6 MB (50.13%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        for(int i=2; i<=n; ++i) {
-            long res = (long)i*((long)i+1)/2;
-            if(res > n) {
+        int num = n;
+        for(int i=1; i<=num; ++i) {
+            // long res = (long)i*((long)i+1)/2;
+            // if(res > n) {
+            //     return i-1;
+            // }else if(res == n) {
+            //     return i;
+            // }
+            if(n < 0) {
+                cout << i-2 << endl;
+                return i-2;
+            }else if(n==0){ 
+                cout << "value of i" <<  i << endl;
                 return i-1;
-            }else if(res == n) {
-                return i;
             }
-        }
 
+            n -= i;
+
+        }
         return 1;
     }
 };
