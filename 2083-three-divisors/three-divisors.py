1900e3164a747a17344af55615ec1fc529f265ff
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-13 21:26:33 +0500

    Time: 59 ms (5.73%) | Memory: 16.7 MB (12.21%) - LeetSync

--- 2083-three-divisors/three-divisors.py
+++ 2083-three-divisors/three-divisors.py
@@ -1,12 +1,16 @@
 class Solution:
     def isThree(self, n: int) -> bool:
-        c = 2
-        s = int(n/2)
-        for i in range(2, s+1):
+        counter = 2
+        half = int(n/2)
+        for i in range(2, half+1):
+            print("value of i : " + str(i))
             if(n%i==0):
-                c = c + 1
-            if (c > 3):
-                break
+                counter = counter + 1
+                print("divide : " + str(i))
+            
+        if(counter==3):
+            return True
+        else:
+            return False
         
-        return c == 3
         
