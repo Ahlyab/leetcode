f2ac797189c89fd8da22196e32969c63d055d0d6
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-13 21:27:20 +0500

    Time: 35 ms (75.16%) | Memory: 16.5 MB (60.19%) - LeetSync

--- 2083-three-divisors/three-divisors.py
+++ 2083-three-divisors/three-divisors.py
@@ -3,10 +3,8 @@ class Solution:
         counter = 2
         half = int(n/2)
         for i in range(2, half+1):
-            print("value of i : " + str(i))
             if(n%i==0):
                 counter = counter + 1
-                print("divide : " + str(i))
             
         if(counter==3):
             return True
