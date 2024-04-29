d69dc795603659b43cb043b02829b0743c85f9d9
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-29 20:16:53 +0500

    Time: 1453 ms (26.37%) | Memory: 22.2 MB (15.82%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -4,15 +4,19 @@ public:
         int res = 0;
         vector<int> divisors;
         for(int i=0; i<nums.size(); ++i) {
-            // if(nums[i] >= 6) {
+            if(nums[i] >= 6) {
+                cout << nums[i] << " : ";
                 divisors = getDivisors(nums[i]);
-            // }
+            }
 
             if(divisors.size() == 4) {
                 for(int j=0; j<4; ++j) {
+                    cout << divisors[j] << " ";
                     res += divisors[j];
                 }
+                divisors.clear();
             }
+            cout << endl;
         }
         return res;
     }
