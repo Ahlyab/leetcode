6dd82a2a3d5022f1e8f88381e3afbfdf18c8db5c
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-04-29 20:08:10 +0500

    Time: 1408 ms (26.76%) | Memory: 22.1 MB (15.82%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int res = 0;
+        for(int i=0; i<nums.size(); ++i) {
+            vector<int> divisors = getDivisors(nums[i]);
+
+            if(divisors.size() == 4) {
+                for(int j=0; j<divisors.size(); ++j) {
+                    res += divisors[j];
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<int> getDivisors(const int &num) {
+        vector<int> res = {1, num};
+        int count = 2;
+
+        for(int i=2; i<=num/2; ++i) {
+            if(num%i == 0) {
+                ++count;
+                res.push_back(i);
+            }
+
+            if(count > 4) {
+                break;
+            }
+        }
+
+        return res;
+    }
+};
