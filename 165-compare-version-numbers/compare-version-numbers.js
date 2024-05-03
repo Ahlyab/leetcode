523f1444c7d71c764d40908abeee3d9389268eee
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-05-03 18:34:39 +0500

    Time: 66 ms (5.38%) | Memory: 50.9 MB (7.69%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
+var compareVersion = function(version1, version2) {
+    const s1 = version1.split('.');
+    const s2 = version2.split('.');
+    console.log(s1, s2);
+
+    const maxLength = Math.max(s1.length, s2.length);
+
+    for(let i = 0; i<maxLength; ++i) {
+        const v1 = i < s1.length ? parseInt(s1[i]) : 0;
+        const v2 = i < s2.length ? parseInt(s2[i]) : 0;
+
+        console.log(v1, v2);
+
+        if(v1 > v2) {
+            return 1;
+        }else if(v1 < v2) {
+            return -1;
+        }
+    }
+        return 0;
+};
+
