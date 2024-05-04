2e0582c5895aea6e884526bfd6cf5c4060a7e2aa
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-05-04 10:45:18 +0500

    Time: 50 ms (89.23%) | Memory: 45.4 MB (85.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -7,16 +7,22 @@ public:
         int i=0, j=people.size()-1;
 
         while(i<=j) {
-            if(people[i] + people[j] == limit) {
-                count++;
-                i++;
-                j--;
-            }else if(people[i] + people[j] > limit){
-                count++;
-                j--;
-            }else if(people[i] + people[j] < limit) {
-                count++;
-                ++i; --j;
+            // if(people[i] + people[j] == limit) {
+            //     count++;
+            //     i++;
+            //     j--;
+            // }else if(people[i] + people[j] > limit){
+            //     count++;
+            //     j--;
+            // }else if(people[i] + people[j] < limit) {
+            //     count++;
+            //     ++i; --j;
+            // }
+            int remain = limit - people[j];
+            --j;
+            count++;
+            if(i <= j && remain >= people[i]) {
+                ++i;
             }
         }
         return count;
