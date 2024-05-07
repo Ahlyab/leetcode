62982bcc0311c100c46120ffce1c19b355465f0b
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-05-07 23:21:25 +0500

    Time: 171 ms (68.98%) | Memory: 120.7 MB (72.53%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
+   ListNode* reverseList(ListNode* head){
+        ListNode* dummy=NULL;
+        while(head!=NULL){
+            ListNode* temp=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=temp;
+        } 
+        return dummy;
+    }
     ListNode* doubleIt(ListNode* head) {
-        // Initialize a stack to store the values of the linked list
-        stack<int> values;
-        int val = 0;
-
-        // Traverse the linked list and push its values onto the stack
-        while (head != nullptr) {
-            values.push(head->val);
-            head = head->next;
+        head=reverseList(head);
+        int carry=0;
+        ListNode* temp=head;
+        ListNode* prev=NULL;
+        while(temp!=NULL){
+            int num=temp->val;
+            temp->val=(num*2+carry)%10;
+            carry=(num*2+carry)/10;
+            prev=temp;
+            temp=temp->next;
         }
 
-        ListNode* newTail = nullptr;
-
-        // Iterate over the stack of values and the carryover
-        while (!values.empty() || val != 0) {
-            // Create a new ListNode with value 0 and the previous tail as its next node
-            newTail = new ListNode(0, newTail);
-            
-            // Calculate the new value for the current node
-            // by doubling the last digit, adding carry, and getting the remainder
-            if (!values.empty()) {
-                val += values.top() * 2;
-                values.pop();
-            }
-            newTail->val = val % 10;
-            val /= 10;
+        if(carry){
+            ListNode* n=new ListNode(carry);
+            prev->next=n;
         }
-
-        // Return the tail of the new linked list
-        return newTail;
+        head=reverseList(head);
+        return head;
+    
     }
 };
