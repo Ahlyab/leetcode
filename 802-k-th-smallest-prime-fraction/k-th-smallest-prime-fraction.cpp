fa3da0e7b7361ab988765e2dd24d2feba155ce25
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-05-10 16:06:52 +0500

    Time: 7 ms (90.33%) | Memory: 10.4 MB (100.00%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        
+        const int n = arr.size();
+    double l = 0.0;
+    double r = 1.0;
 
-        map<double, vector<int>> record;
+    while (l < r) {
+      const double m = (l + r) / 2.0;
+      int fractionsNoGreaterThanM = 0;
+      int p = 0;
+      int q = 1;
 
-        for(int i=0;i<arr.size(); ++i) {
-            for(int j=0; j<i; ++j) {
-                double id = (double)arr[j]/arr[i];
-                record[id] = vector<int>{arr[j], arr[i]};
-            }
+      // For each index i, find the first index j s.t. arr[i] / arr[j] <= m,
+      // so fractionsNoGreaterThanM for index i will be n - j.
+      for (int i = 0, j = 1; i < n; ++i) {
+        while (j < n && arr[i] > m * arr[j])
+          ++j;
+        if (j == n)
+          break;
+        fractionsNoGreaterThanM += n - j;
+        if (p * arr[j] < q * arr[i]) {
+          p = arr[i];
+          q = arr[j];
         }
+      }
 
-        int i = 0;
+      if (fractionsNoGreaterThanM == k)
+        return {p, q};
+      if (fractionsNoGreaterThanM > k)
+        r = m;
+      else
+        l = m;
+    }
 
-        for(auto value : record) {
-            ++i;
-            if(i == k) {
-                return value.second;
-            }
-        }
-        return arr;
+    throw;
     }
 };
