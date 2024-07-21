fe288e7a3aeb579b665f4c1b6833dc3657109aa9
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-07-21 23:34:53 +0500

    Time: 3 ms (38.47%) | Memory: 8.2 MB (19.42%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if( n < 0) {
+           double res =  helper(x, static_cast<long>(abs(n)));
+           return 1 / res;
+        }
+        return helper(x, n);
+    }
+
+    double helper(double x, long n) {
+        if(n==0)
+            return 1;
+        if(n%2==0)
+            return helper(x*x, n/2);
+        
+        return x * helper(x*x, (n-1)/2);
     }
 };
