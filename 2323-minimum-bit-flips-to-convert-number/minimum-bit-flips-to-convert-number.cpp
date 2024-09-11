b62be35181282a8c70984c7fb03b8602a6d10e34
Ahalyab Asad <44720995+Ahlyab@users.noreply.github.com>
2024-09-11 16:47:02 +0500

    Time: 5 ms (41.47%) | Memory: 7.8 MB (44.87%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        
+        string s = bitset<32>(start).to_string(); // 01100100
+        string target = bitset<32>(goal).to_string();
+        int count = 0;
+
+        int i_start = s.find('1');
+        int s_target = target.find('1');
+
+        if(i_start == string::npos){
+            i_start = 32;
+        }else if(s_target == string::npos) {
+            s_target = 32;
+        }
+
+        cout << i_start << " : " << s_target << endl;
+        cout << s << " : " << target << endl;
+
+        for(int i=min(i_start, s_target); i < s.size(); ++i) {
+            if(s[i] != target[i]) {
+                ++count;
+            }
+        }
+        return count;
+    }
+};
